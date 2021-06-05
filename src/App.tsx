f1d043c84fd2b7afab805d42751ad4fb7fd3863b
milkcask <1572863+milkcask@users.noreply.github.com>
2021-06-05 21:12:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider, styled} from 'baseui';
+
+import {
+  HeaderNavigation,
+  ALIGN,
+  StyledNavigationItem as NavigationItem,
+  StyledNavigationList as NavigationList,
+} from 'baseui/header-navigation';
+
+import SearchBox from './SearchBox';
+import MovieSearchResults from './MovieSearchResults';
+
+const engine = new Styletron();
 
 function App() {
+  const [movies, setMovies] = React.useState<Movie[]>([]);
+  const [firstSearch, setfirstSearch] = React.useState<Boolean>(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <HeaderNavigation>
+          <NavigationList $align={ALIGN.left} style={{width: '100%'}}>
+            <NavigationItem>
+              search-OMDb
+            </NavigationItem>
+            <NavigationItem style={{width: '100%'}}>
+              <SearchBox setMovies={setMovies} setfirstSearch={setfirstSearch} />
+            </NavigationItem>
+          </NavigationList>
+        </HeaderNavigation>
+        { firstSearch ? (
+          <MovieSearchResults movies={movies} />
+        ):(null)}
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
